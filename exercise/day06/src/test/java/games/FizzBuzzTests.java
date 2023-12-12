7dfc904a705b70ad13e534880f7ea0381376293d
Gildas Quéméner <gildas.quemener@gmail.com>
2023-12-12 08:46:36 +0100

    day06: improve test readablity

--- exercise/day06/src/test/java/games/FizzBuzzTests.java
+++ exercise/day06/src/test/java/games/FizzBuzzTests.java
@@ -1,87 +1,89 @@
 package games;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class FizzBuzzTests {
-    public static IntStream notMultipleOf3Nor5() {
-        return IntStream
-                .range(1, 100)
-                .filter((i) -> i % 3 != 0) // TODO: replace with partial application of a multipleOf function
-                .filter((i) -> i % 5 != 0);
-    }
 
-    public static IntStream onlyMultiplesOf3() {
-        return IntStream
-                .range(1, 100)
-                .filter((i) -> i % 3 == 0) // TODO: replace with partial application of a multipleOf function
-                .filter((i) -> i % 5 != 0);
-    }
-
-    public static IntStream onlyMultiplesOf5() {
-        return IntStream
-                .range(1, 100)
-                .filter((i) -> i % 5 == 0) // TODO: replace with partial application of a multipleOf function
-                .filter((i) -> i % 3 != 0);
-    }
-
-    public static IntStream multipleOf3And5() {
-        return IntStream
-                .range(1, 100)
-                .filter((i) -> i % 5 == 0) // TODO: replace with partial application of a multipleOf function
-                .filter((i) -> i % 3 == 0);
-    }
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 100;
 
     @ParameterizedTest
     @MethodSource("notMultipleOf3Nor5")
     void returns_the_given_number_for_non_multiple_of_3_nor_5(Integer i) throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(i))
-                .isEqualTo(String.valueOf(i));
+        assertThat(FizzBuzz.convert(i)).isEqualTo(String.valueOf(i));
     }
 
     @ParameterizedTest
     @MethodSource("onlyMultiplesOf3")
     void returns_Fizz_for_multiple_of_3(Integer i) throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(i))
-                .isEqualTo("Fizz");
+        assertThat(FizzBuzz.convert(i)).isEqualTo("Fizz");
     }
 
     @ParameterizedTest
     @MethodSource("onlyMultiplesOf5")
     void returns_Buzz_for_multiple_of_5(Integer i) throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(i))
-                .isEqualTo("Buzz");
+        assertThat(FizzBuzz.convert(i)).isEqualTo("Buzz");
     }
 
     @ParameterizedTest
     @MethodSource("multipleOf3And5")
     void returns_FizzBuzz_for_multiple_of_3_and_5(Integer i) throws OutOfRangeException {
-        assertThat(FizzBuzz.convert(i))
-                .isEqualTo("FizzBuzz");
+        assertThat(FizzBuzz.convert(i)).isEqualTo("FizzBuzz");
     }
 
     @Test
-    void throws_an_exception_for_0() {
-        assertThatThrownBy(() -> FizzBuzz.convert(0))
-                .isInstanceOf(OutOfRangeException.class);
+    void throws_an_exception_for_too_small_value() {
+        assertThatThrownBy(() -> FizzBuzz.convert(MIN_VALUE - 1)).isInstanceOf(OutOfRangeException.class);
     }
 
     @Test
-    void throws_an_exception_for_101() {
-        assertThatThrownBy(() -> FizzBuzz.convert(101))
-                .isInstanceOf(OutOfRangeException.class);
+    void throws_an_exception_for_too_big_value() {
+        assertThatThrownBy(() -> FizzBuzz.convert(MAX_VALUE + 1)).isInstanceOf(OutOfRangeException.class);
     }
 
     @Test
     void throws_an_exception_for_minus_1() {
-        assertThatThrownBy(() -> FizzBuzz.convert(-1))
-                .isInstanceOf(OutOfRangeException.class);
+        assertThatThrownBy(() -> FizzBuzz.convert(-1)).isInstanceOf(OutOfRangeException.class);
+    }
+
+    static IntStream notMultipleOf3Nor5() {
+        return validRange().filter(not(multipleOf(3)).and(not(multipleOf(5))));
+    }
+
+    static IntStream onlyMultiplesOf3() {
+        return validRange().filter(multipleOf(3).and(not(multipleOf(5))));
+    }
+
+    static IntStream onlyMultiplesOf5() {
+        return validRange().filter(multipleOf(5).and(not(multipleOf(3))));
+    }
+
+    static IntStream multipleOf3And5() {
+        return validRange().filter(multipleOf(3).and(multipleOf(5)));
+    }
+
+    @Contract(pure = true)
+    private static @NotNull IntStream validRange() {
+        return IntStream.range(MIN_VALUE, MAX_VALUE);
+    }
+
+    @Contract(pure = true)
+    private static @NotNull IntPredicate multipleOf(Integer d) {
+        return (i) -> i % d == 0;
+    }
+
+    @Contract(pure = true)
+    private static @NotNull IntPredicate not(@NotNull IntPredicate p) {
+        return p.negate();
     }
 }
